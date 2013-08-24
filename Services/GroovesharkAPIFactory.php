b2e6bade1dde05742a928485e3cc3df6375b8d19
plfort <plfort@cogipix.com>
2013-08-24 12:14:35 +0200

    add error logging

--- Services/GroovesharkAPIFactory.php
+++ Services/GroovesharkAPIFactory.php
@@ -24,16 +24,21 @@ class GroovesharkAPIFactory{
         if($this->gsApi === null){
             $this->gsApi= new $this->apiClass($this->key,$this->secret);
             $session=$this->container->get('session');
-            if ($session->get('gsSession',false)) {
-                $this->gsApi->setSession($session->get('gsSession'));
-            } else {
-                $session->set('gsSession',$this->gsApi->startSession());
-            }
-            if ($session->get('gsCountry',false)) {
-                $this->gsApi->setCountry($session->get('gsCountry',false));
-            } else {
-                $session->set('gsCountry',$this->gsApi->getCountry());
+            try{
 
+                if ($session->get('gsSession',false)) {
+                    $this->gsApi->setSession($session->get('gsSession'));
+                } else {
+                    $session->set('gsSession',$this->gsApi->startSession());
+                }
+                if ($session->get('gsCountry',false)) {
+                    $this->gsApi->setCountry($session->get('gsCountry',false));
+                } else {
+                    $session->set('gsCountry',$this->gsApi->getCountry());
+
+                }
+            }catch(\Exception $ex){
+                $this->container->get('logger')->err($ex);
             }
         }
        return $this->gsApi;
