df3cd3d3d30e7ef41a630ffc1156b16aeb77cfaf
plfort <plfort@cogipix.com>
2014-03-09 23:30:56 +0100

    new menu

--- ViewHooks/Menu/MenuItem.php
+++ ViewHooks/Menu/MenuItem.php
@@ -1,17 +1,28 @@
 <?php
 namespace Cogipix\CogimixGroovesharkBundle\ViewHooks\Menu;
 use Cogipix\CogimixCommonBundle\ViewHooks\Menu\MenuItemInterface;
+use Cogipix\CogimixCommonBundle\ViewHooks\Menu\AbstractMenuItem;
 
 /**
  *
  * @author plfort - Cogipix
  *
  */
-class MenuRenderer implements MenuItemInterface{
+class MenuItem extends AbstractMenuItem
+{
 
     public function getMenuItemTemplate()
     {
           return 'CogimixGroovesharkBundle:Menu:menu.html.twig';
 
     }
+
+	 /* (non-PHPdoc)
+	  * @see \Cogipix\CogimixCommonBundle\ViewHooks\Menu\MenuItemInterface::getName()
+	  */
+	 public function getName() {
+	 	return 'grooveshark';
+
+	 }
+
 }
